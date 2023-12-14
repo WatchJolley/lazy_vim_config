c11ce1570203ebbb5ba85ee3b2c93a7f7328e921
WatchJolley <ben.jolley21@googlemail.com>
2023-12-14 20:39:46 +0000

    Add Mason config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "pyright",
+        "black",
+        "isort",
+        "mypy",
+        "ruff",
+      },
+    },
+  },
+}
